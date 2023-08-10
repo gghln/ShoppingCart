2d4f65d8b0bb2b196ff39ae76dc817af4825153f
gghln <61983256+gghln@users.noreply.github.com>
2023-08-11 01:47:55 +0300

    Fixed Card component rendering

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -4,31 +4,19 @@ import Card from './Components/Card'
 
 function App() {
   const [products,setProducts] = useState([])
+  
   useEffect(() => {
     fetch('https://fakestoreapi.com/products/category/electronics')
       .then(res=>res.json())
       .then(json=>setProducts(json))
       .catch(error => console.error(error))
   },[])
-  console.log(products)
   
   return (
     <div className='grid-container'>
       {
         products.map((product) => {
-          return( 
-            <div className="card-container" key={product.id}>
-                <img className="product-image" src={product.image}/>
-                <h3 className="product-name">{product.title}</h3>
-                <h5 className="product-price">{product.price}</h5>
-                <div className='product-quantity-container'>
-                    <button className="minus-button">-</button>
-                    <input type="text" className="product-quantity"/>
-                    <button className="plus-button">+</button>
-                </div>
-                <button className='Add-to-cart'>Add to Cart</button>
-            </div>    
-        )
+          return(<Card {...product} key={product.id}/>)
         })
       }
     </div>
--- shopping-cart/src/Components/Card.jsx
+++ shopping-cart/src/Components/Card.jsx
@@ -1,16 +1,17 @@
 import './styles/Card.css'
 
-const Card = () => {
-    const{id,title,image,price} = props
-    console.log(id,title,image,price)
+const Card = (props) => {
+    // eslint-disable-next-line react/prop-types
+    const{title,image,price} = props
+
     return( 
-        <div className="card-container" key={id}>
+        <div className="card-container">
             <img className="product-image" src={image}/>
             <h3 className="product-name">{title}</h3>
-            <h5 className="product-price">{price}</h5>
+            <h5 className="product-price">{price} €</h5>
             <div className='product-quantity-container'>
                 <button className="minus-button">-</button>
-                <input type="text" className="product-quantity" value="1"/>
+                <input type="text" className="product-quantity"/>
                 <button className="plus-button">+</button>
             </div>
             <button className='Add-to-cart'>Add to Cart</button>
